c7ee9d45ef55d92822209bb979de8887e3f0616a
cosseal <56023332+cosseal@users.noreply.github.com>
2019-12-06 11:47:25 +0100

    le pendu blocage inner html

--- script.js
+++ script.js
@@ -6,11 +6,10 @@ var tabMots = [
 ];
 var alea = Math.floor(Math.random()*tabMots.length);
 var selAlea =tabMots [alea];
-var lettre = document.getElementById("lettre").value;
 var nbLettres = selAlea.length;
 var submit = document.getElementById("submit");
 var aff = document.getElementById("mad");
-
+var i;
 
 console.log (selAlea);
 console.log (nbLettres);
@@ -19,19 +18,31 @@ function affNbLettres () {
     aff.innerHTML = "";
 
     for (i = 0; i<nbLettres; i++) {
-        aff += "_";
+        aff.innerHTML += " _ ";
     }
-
 }
+
 affNbLettres();
 
 submit.addEventListener("click", function () {
+    var lettre = document.getElementById("lettre");
+    var posLettre = selAlea.indexOf(lettre);
+    // posLettre [i].innerHTML = lettre;
 
+        for ( i=0; i<nbLettres;i++) {
+            if (selAlea [i] === lettre.value) {
 
-})
+// ma logique est de rechercher à ce moment là l'index/les index correspondant à la valeur de ma lettre.
+// et donc de créer dans ma boucle, ma var posLettre qui recherche l'index.
+// et ensuite, de changer l'inner HTML de ma variable aff en y remplaçant le texte _ par la valeur de ma lettre
+// mais je n'y parvient pas...un coup de main? un indice pour avancer? ma logique est-elle bonne?
 
-function comparer() {
+                console.log (posLettre);
 
+            }
 
-    
-}
+            else {}
+
+            }
+    });
+
